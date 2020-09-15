8c475a14dbf7cd73d7e8afe0e6f2163458ffad73
Rodrigo Poma <rodpoma@gmail.com>
2020-09-14 18:37:44 -0600

    Component structuring

---
+++ src/components/Landing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Landing = () => {
+  return <div>Landing spot</div>;
+};
+
+export default Landing;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Nav = () => {
+  return <div>SideBar</div>;
+};
+
+export default Nav;
