b80fd514cd6880dc52bb2b97c89cf9d714b50121
Rodrigo Poma <rodpoma@gmail.com>
2020-09-16 13:33:37 -0600

    Navbar & Footer complete minus animation

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import {Container} from "react-bootstrap";
 import {BrowserRouter, Route} from "react-router-dom";
 
 import "./App.css";
+import "mdbreact/dist/css/mdb.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const App = () => {
   return (
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import {
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBNavLink,
+  MDBIcon,
+} from "mdbreact";
+import PWL from "./assets/Portfolio website logo.png";
 
-const NavBar = () => {
-  return <div>NavBar</div>;
+const Navbar = () => {
+  return (
+    <MDBNavbar color="blue-gradient" dark expand="md">
+      <MDBNavbarBrand href="/" className="ml-5">
+        <img src={PWL} alt="" height="60" />
+      </MDBNavbarBrand>
+      <MDBNavbarNav right>
+        <MDBNavItem>
+          <MDBNavLink className="white-text ml-2 mr-5" to="/about">
+            <MDBIcon icon="user-astronaut" className="white-text" size="3x" />
+          </MDBNavLink>
+        </MDBNavItem>
+        <MDBNavItem>
+          <MDBNavLink className="white-text ml-2 mr-5" to="/skills">
+            <MDBIcon icon="tools" className="white-text" size="3x" />
+          </MDBNavLink>
+        </MDBNavItem>
+        <MDBNavItem>
+          <MDBNavLink className="white-text ml-2 mr-5" to="/projects">
+            <MDBIcon icon="code" className="white-text" size="3x" />
+          </MDBNavLink>
+        </MDBNavItem>
+        <MDBNavItem>
+          <MDBNavLink className="white-text ml-2 mr-5" to="/contact">
+            <MDBIcon icon="envelope" className="white-text" size="3x" />
+          </MDBNavLink>
+        </MDBNavItem>
+      </MDBNavbarNav>
+    </MDBNavbar>
+  );
 };
 
-export default NavBar;
+export default Navbar;
